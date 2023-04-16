3de16260614d860bb42acc3aa961516afc8bea28
rs <renfreedom@foxmail.com>
2023-04-16 20:39:56 +0800

    test

--- docker.sh
+++ docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --gpus=all --net=host -v $PWD:/mmaction2 -w /mmaction2 mmaction2:v1.0.0-cuda11.1
+docker run -it --rm --gpus=all --net=host -v $PWD:/mmaction2 -w /mmaction2 shuairen/mmaction2:v1.0.0-cuda11.1
